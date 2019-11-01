377bacfaebd88ad0aa77202856b778f4795e26ea
lanjing <lanj163@163.com>
2019-11-01 20:11:57 +0800

    first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="demo">
+    {{demo}}
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        demo: 'index'
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .demo {
+    color: red;
+  }
+</style>
